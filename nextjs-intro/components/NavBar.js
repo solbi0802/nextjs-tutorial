7c1ebab74b4afb030b416f68e2fc2905e034a5ca
SolbiShin <sbshin.dev@gmail.com>
2022-01-25 21:53:42 +0900

    styles jsx

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -1,29 +1,27 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NaVBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
       <Link href="/">
-        <a
-          className={`${styles.link} ${
-            router.pathname === "/" ? styles.active : ""
-          }`}
-        >
-          Home
-        </a>
+        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
       </Link>
       <Link href="/about">
-        <a
-          className={`${styles.link} ${
-            router.pathname === "/about" ? styles.active : ""
-          }`}
-        >
-          About
-        </a>
+        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
       </Link>
+      <style jsx>{`
+        nav {
+          background-color: pink;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: gray;
+        }
+      `}</style>
     </nav>
   );
 }
