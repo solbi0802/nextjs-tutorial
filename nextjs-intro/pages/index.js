1ad9d50bb477e28eced43d1a038408fa5ba7d90c
SolbiShin <sbshin.dev@gmail.com>
2022-01-31 11:01:26 +0900

    Redirect and Rewrite

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,32 +1,47 @@
 import { useState, useEffect } from "react";
 import Seo from "../components/Seo";
 
-const API_KEY = "727117319f1f914f942dd8a8ea925bf9";
-
 export default function Home() {
   const [movies, setMovies] = useState();
 
   useEffect(() => {
     (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
+      const { results } = await (await fetch(`/api/movies`)).json();
       setMovies(results);
     })();
   }, []);
 
   return (
-    <div>
+    <div className="container">
       <Seo title="home" />
       {!movies && <h4>Loading....</h4>}
       {movies?.map((movie) => (
         <div key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
-      <h1 className="active">Hello World!</h1>
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
