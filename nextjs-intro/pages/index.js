fd0b691ed583323de72766f8b7738ba2123bd0df
SolbiShin <sbshin.dev@gmail.com>
2022-02-01 23:56:57 +0900

    movie image click 스타일링

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -6,8 +6,8 @@ export default function Home({ results }) {
     <div className="container">
       <Seo title="home" />
       {results?.map((movie) => (
-        <div key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
@@ -17,6 +17,9 @@ export default function Home({ results }) {
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
